e000b7f7379338aa692d316ef0c4ef44d2c53ca5
iam149cm <85501501+iam149cm@users.noreply.github.com>
2022-12-08 20:52:23 +0900

    view update

--- 4_TodoList/app.js
+++ 4_TodoList/app.js
@@ -10,6 +10,7 @@ const _ = require("lodash");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
